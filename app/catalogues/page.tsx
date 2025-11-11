13d36916cb22bef8060d3667c7a77bfebc762c2d
Hachem Ouanes <hachem.ouanes@gmail.com>
2025-11-11 15:35:34 +0100

    fix: revalidation error catalgues

--- app/catalogues/page.tsx
+++ app/catalogues/page.tsx
@@ -4,6 +4,9 @@ import { Footer } from '@/components/shared/footer'
 import { CataloguesPageClient } from '@/components/catalogues/catalogues-page-client'
 import { getAllCatalogues } from '@/services/sanity/catalogues'
 
+// Revalidate every 10 seconds
+export const revalidate = 10
+
 export const metadata: Metadata = {
   title: 'Catalogues',
   description: 'Consultez nos catalogues produits et documentations techniques.'
